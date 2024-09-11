af02b3cadeedf013573ddd74adbefae782d1f50c
mbuthia77 <chege.mc@gmail.com>
2024-09-11 16:22:00 +0300

    Added finance_calculator.py
    Objective: Use user input, variables, and arithmetic operations to calculate and provide feedback on a user’s monthly savings and potential future savings without applying conditional statements.
    
    Task Description:
    
    You will create a script named finance_calculator.py. This script will calculate the user’s monthly savings based on inputted monthly income and expenses. It will then project these savings over a year, assuming a fixed interest rate, to demonstrate compound interest’s effect on savings.

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+income = int(input("Enter your monthly income: "))
+expenses = int(input("Enter your monthly expenses: "))
+savings = income - expenses
+yearly_savings = savings * 12 + (savings * 12 * 0.05)
+print("Your monthly savings are", savings)
+print("Projected savings after one year, with interest, is:", yearly_savings)
