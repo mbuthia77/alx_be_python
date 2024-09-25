565ac454353a5464cd0c845f6c70592652cc6be7
mbuthia77 <chege.mc@gmail.com>
2024-09-25 20:11:54 +0300

    Delete fns_and_dsa/arithmetic_operations.py.py

--- fns_and_dsa/arithmetic_operations.py.py
+++
@@ -1,8 +0,0 @@
-number1 = 10
-number2 = 5
-add = number1 + number2
-sub = number1 - number2
-mul = number1 * number2
-print("Addition of", number2, "and", number1, "is", add)
-print("Subtraction of", number1, "and", number2, "is", sub)
-print("Multiplication of", number1, "and", number2, "is", mul)
