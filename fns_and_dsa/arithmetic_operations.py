efd97286e69ac56070110bd1d27c8c3cd118773f
mbuthia77 <chege.mc@gmail.com>
2024-09-26 15:37:54 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,5 +7,6 @@ def perform_operation(num1, num2, operator):
         return num1 + num2
     elif operator == 'divide':
         if num2 !=0: 
-            print("The result is ", num1 / num2)
+            return "The result is ", num1 / num2
         else: print("Cannot divide by zero.")
+    else: return "Error: Invalid operation"
