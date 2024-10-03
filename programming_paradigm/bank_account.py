4209ca49f8de65e60c5d723c9b1513fabfce0201
mbuthia77 <chege.mc@gmail.com>
2024-10-03 14:47:06 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,7 +14,7 @@ class BankAccount:
     
     
     def display_balance(self):
-        print(self.account_balance)
+        print("Current Balance: $", self.account_balance)
 
 #Testing code
 #Chege = BankAccount(100)
