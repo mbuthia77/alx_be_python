0d7c5050ea88dd2dd742077c4171ad34c0ac9bc2
mbuthia77 <chege.mc@gmail.com>
2024-10-11 13:46:04 +0300

    Added files

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,31 @@
+import math
+from abc import ABC, abstractmethod
+
+class Shape: 
+    @abstractmethod
+    def area(self):
+        raise NotImplementedError
+    
+class Rectangle(Shape):
+    
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    
+    
+    def area(self):
+        return self.length * self.width
+        
+class Circle(Shape): 
+    
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+         return math.pi * self.radius * self.radius
+
+
+#carton = Rectangle(3, 6)
+#ball = Circle(7)
+
+#print(f"Area of carton is {carton.area()} & area of ball is {ball.area()}")
